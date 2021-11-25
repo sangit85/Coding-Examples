6dbad5c0c01e2ac3498daa6b8b7328cb63f4e447
sangit85 <santoshk.github@gmail.com>
2021-11-25 09:45:27 +0530

    commit
    commy

---
+++ OneToManyRelationship/src/main/java/com/Question.java
@@ -0,0 +1,37 @@
+package com;  
+import javax.persistence.*;
+import java.util.List;  
+
+@Entity
+@Table(name="Questiontable")
+public class Question {  
+
+@Id	
+@GeneratedValue
+private int id;  
+private String qname;  
+
+@OneToMany(cascade = CascadeType.PERSIST)
+@JoinColumn(name="qid")
+@OrderColumn(name="type")
+
+private List<Answer> answers;
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getQname() {
+	return qname;
+}
+public void setQname(String qname) {
+	this.qname = qname;
+}
+public List<Answer> getAnswers() {
+	return answers;
+}
+public void setAnswers(List<Answer> answers) {
+	this.answers = answers;
+}    
+}  
