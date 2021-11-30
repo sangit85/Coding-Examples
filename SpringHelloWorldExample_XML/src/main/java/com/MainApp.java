5c46274c543451b60a8bf08d20084982574f853b
sangit85 <santoshk.github@gmail.com>
2021-11-30 10:57:17 +0530

    commit
    SpringHelloWorld_XML Example

---
+++ SpringHelloWorldExample_XML/src/main/java/com/MainApp.java
@@ -0,0 +1,14 @@
+package com;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+   public static void main(String[] args)
+   {
+      ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+      // Context object is a spring container
+      HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
+      obj.getMessage();
+   }
+}
