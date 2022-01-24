38cfa960264db4d968473ab8f4d160ad162aa20c
sangit85 <santoshk.github@gmail.com>
2022-01-24 17:01:36 +0530

    commit
    commit

---
+++ JDBCDAOExample/src/com/MainClass.java
@@ -0,0 +1,23 @@
+package com;
+
+public class MainClass {
+	public static void main(String[] args) {
+		StudentJDBCDAO student = new StudentJDBCDAO();
+		StudentBean alok = new StudentBean();
+		alok.setName("Alok");
+		alok.setRollNo(9);
+		alok.setCourse("MBA");
+		alok.setAddress("Ranchi");
+		StudentBean tinkoo = new StudentBean();
+		tinkoo.setName("Arvind");
+		tinkoo.setRollNo(6);
+		// Adding Data
+		student.add(alok);
+		// Deleting Data
+		student.delete(7);
+		// Updating Data
+		student.update(tinkoo);
+		// Displaying Data
+		student.findAll();
+	}
+}
